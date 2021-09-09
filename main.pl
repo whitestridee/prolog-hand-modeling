ebfe098c9ffcb69e21024709d768e5187ffc7cc1
whitestridee <borislav.dedic77@gmail.com>
2021-09-09 10:48:09 +0300

    Finished finger validation

--- main.pl
+++ main.pl
@@ -2,13 +2,12 @@
 use_module(helper),
 use_module(validation).
 
-mainfile:-
+%Вызываем эту фигню с аргументами
+mainfile(Interval, Time):-
 %Тут надо встасить ссылку на свою папку с проектом
 working_directory(_, "C:/Users/green/Documents/GitHub/prolog-hand-modeling"),
-read_files:loadpoint(Lines, 'xyz_1_5.txt', 1),
-helper:getfirstel(Lines,Value),
-read_files:loadpoint(Point1, 'xyz_1_13.txt', 1),
-read_files:loadpoint(Point2, 'xyz_1_14.txt', 1),
-read_files:loadpoint(Point3, 'xyz_1_15.txt', 1),
+read_files:tryloadpoint(Point1, 1, 3, 1),
+read_files:tryloadpoint(Point2, 1, 5, 1),
+read_files:tryloadpoint(Point3, 1, 7, 1),
 validation:validatefinger(Point1,Point2,Point3),
-write(Value).
+validation:validateallfingers(Interval,Time).
--- read_files.pl
+++ read_files.pl
@@ -1,17 +1,26 @@
-:- module(read_files,[loadfile/2, loadpoint/3]).
+:- module(read_files,[loadfile/2, loadpoint/3,tryloadpoint/4]).
 
 loadfile(Lines,Filename) :-
     string_concat("points_data_with_dots/",Filename,Full),
-    open(Full, read, Str),%Надо поменять working_directory на свой...
+    open(Full, read, Str),
     read_file(Str,Lines),
     close(Str).
 
-loadpoint(Line, Filename, PointNumber) :-
+loadpoint(Line, Filename, PointTime) :-
     string_concat("points_data_with_dots/",Filename,Full),
-    open(Full, read, Str),%Надо поменять working_directory на свой...
+    open(Full, read, Str),
     CurNumber = 0,
-    read_line(PointNumber, CurNumber, Str, Line),
+    read_line(PointTime, CurNumber, Str, Line),
     close(Str).
+
+tryloadpoint(Line, Interval, PointNumber, PointTime) :-
+    name_creator(Interval, PointNumber, Filename) ->
+    open(Filename, read, Str),
+    CurNumber = 0,
+    read_line(PointTime, CurNumber, Str, Line),
+    close(Str);
+    Line = [_1, _1, _1],write("File not found"), nl.
+
 
 read_file(Stream, Lines) :-
     read(Stream, Line),               % Читаем строку
@@ -28,3 +37,12 @@ read_line(PointNumber, CurNumber, Stream, Coordinates):-
     ;  (CurNumber =:= PointNumber) -> Coordinates = Line;       % Иначе добавляем строку и читаем дальше...
        read_line(PointNumber, CurNumber + 1, Stream, Coordinates)
     ).
+%Создаем имя файла...
+name_creator(Interval, Point, FullName):-
+    string_concat("points_data_with_dots/", "xyz_", Var1),
+    string_concat(Var1, Interval, Var2),
+    string_concat(Var2, "_", Var3),
+    string_concat(Var3, Point, Var4),
+    string_concat(Var4, ".txt", TryName),
+    write(TryName),nl,
+    exists_file(TryName) -> FullName = TryName.
