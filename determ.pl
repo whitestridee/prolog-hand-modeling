a74fbcf779d26f6a073bc5029e733234fce8fb79
Anton Sorokin <sgcube@yandex.ru>
2021-09-13 09:22:26 +0300

    template points list construction

--- determ.pl
+++ determ.pl
@@ -1,10 +1,21 @@
-:- module(determ,[determallfingers/3,get_point/4,det_coords/10,determinefinger/5]).
+:- module(determ,[determallfingers/3,get_coords/4,det_angle/2,
+	point_constraint/3,coord_constraint/1,
+	det_coords/11,determinefinger/7]).
  :- use_module(read_files),
    use_module(validation),
    use_module(helper),
-   use_module(library(simplex)).
+   use_module(library(clpqr)),
+   use_module(library(lists)).
    
-determallfingers(Interval, Time, PointList):-
+%determallfingers - call this to determine missing points
+determallfingers(Interval, Time, [
+		Coords1, Coords2, Coords3, Coords4, Coords5, Coords6, Coords7,
+		Coords8, Coords9, Coords10, Coords11, Coords12, Coords13, Coords14,
+		Coords15, Coords16, Coords17, Coords18, Coords19, Coords20, Coords21,
+		Coords22, Coords23, Coords24, Coords25, Coords26, Coords27, Coords28,
+		Coords29, Coords30, Coords31, Coords32, Coords33, Coords34, Coords35,
+		Coords36, Coords37, Coords38, Coords39, Coords40, Coords41, Coords42
+	]):-
 	read_files:tryloadpoint(Point1, Interval, 1, Time),
 	read_files:tryloadpoint(Point2, Interval, 2, Time),
 	read_files:tryloadpoint(Point3, Interval, 3, Time),
@@ -48,97 +59,138 @@ determallfingers(Interval, Time, PointList):-
 	read_files:tryloadpoint(Point41, Interval, 41, Time),
 	read_files:tryloadpoint(Point42, Interval, 42, Time),
 
-	PList=[],
-	determinefinger(Point1, Point2, Point3, bpabc, PList),
-	determinefinger(Point4, Point5, Point6, oabc, PList),
-	determinefinger(Point5, Point6, Point7, obcd, PList),
-	determinefinger(Point8, Point9, Point10, oabc, PList),
-	determinefinger(Point9, Point10, Point11, obcd, PList),
-	determinefinger(Point12, Point13, Point14, oabc, PList),
-	determinefinger(Point13, Point14, Point15, obcd, PList),
-	determinefinger(Point16, Point17, Point18, oabc, PList),
-	determinefinger(Point17, Point18, Point19, obcd, PList),
+	determinefinger(Point1, Point2, Point3, bpabc, Coords1, Coords2, Coords3),
+	determinefinger(Point4, Point5, Point6, oabc, Coords4, Coords5, Coords6),
+	determinefinger(Point5, Point6, Point7, obcd, Coords5, Coords6, Coords7),
+	determinefinger(Point8, Point9, Point10, oabc, Coords8, Coords9, Coords10),
+	determinefinger(Point9, Point10, Point11, obcd, Coords9, Coords10, Coords11),
+	determinefinger(Point12, Point13, Point14, oabc, Coords12, Coords13, Coords14),
+	determinefinger(Point13, Point14, Point15, obcd, Coords13, Coords14, Coords15),
+	determinefinger(Point16, Point17, Point18, oabc, Coords16, Coords17, Coords18),
+	determinefinger(Point17, Point18, Point19, obcd, Coords17, Coords18, Coords19),
 
-	determinefinger(Point22, Point23, Point24, bpabc, PList),
-	determinefinger(Point25, Point26, Point27, oabc, PList),
-	determinefinger(Point26, Point27, Point28, obcd, PList),
-	determinefinger(Point29, Point30, Point31, oabc, PList),
-	determinefinger(Point30, Point31, Point32, obcd, PList),
-	determinefinger(Point33, Point34, Point35, oabc, PList),
-	determinefinger(Point34, Point35, Point36, obcd, PList),
-	determinefinger(Point37, Point38, Point39, oabc, PList),
-	determinefinger(Point38, Point39, Point40, obcd, PList),
-	PointList=PList.
-	
-	
-get_point(Point, X, Y, Z) :-
+	determinefinger(Point22, Point23, Point24, bpabc, Coords22, Coords23, Coords24),
+	determinefinger(Point25, Point26, Point27, oabc, Coords25, Coords26, Coords27),
+	determinefinger(Point26, Point27, Point28, obcd, Coords26, Coords27, Coords28),
+	determinefinger(Point29, Point30, Point31, oabc, Coords29, Coords30, Coords31),
+	determinefinger(Point30, Point31, Point32, obcd, Coords30, Coords31, Coords32),
+	determinefinger(Point33, Point34, Point35, oabc, Coords33, Coords34, Coords35),
+	determinefinger(Point34, Point35, Point36, obcd, Coords34, Coords35, Coords36),
+	determinefinger(Point37, Point38, Point39, oabc, Coords37, Coords38, Coords39),
+	determinefinger(Point38, Point39, Point40, obcd, Coords38, Coords39, Coords40).
+
+%get_coords - set X,Y,Z if Point exists
+get_coords(Point, X, Y, Z) :-
 	validation:checkforstr(Point),
 	helper:getfirstel(Point, X),
 	helper:getnthel(Point, Y, 1),
 	helper:getnthel(Point, Z, 2).
 	
+%det_angle - constraints for angle based on finger type
+det_angle(bpabc, Angle):- Angle #=< 80, Angle #>= -80.
+det_angle(bpbcd, Angle):- Angle #=< 50, Angle #>= -50.
+det_angle(oabc, Angle):- Angle #=< 80, Angle #>= -80.
+det_angle(obcd, Angle):- Angle #=< 100, Angle #>= -100.
+det_angle(ocde, Angle):- Angle #=< 90, Angle #>= -90.
 
-det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
-	gen_state(Ugol),
-	angle_constraint(Type, Ugol),
-	helper:getugol(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Ugol).
+%dummy to test points list construction
+coord_constraint(X) :- 
+	X #>= 0, X #=<2000.
+
+%dummy to test points list construction
+point_constraint(X, Y, Z) :-
+	coord_constraint(X),
+	coord_constraint(Y),
+	coord_constraint(Z).
 	
-angle_constraint -->
-	constraint(validation:checkugol(x1, x2)).
+%det_coords - determination of coordinates of missing points
+
+det_coords(1, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X1,Y1,Z1).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	get_point(Point1, X1, Y1, Z1),
-	get_point(Point2, X2, Y2, Z2),
-	get_point(Point3, X3, Y3, Z3),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(2, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X2,Y2,Z2).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point1, _, _, _)),
-	get_point(Point2, X2, Y2, Z2),
-	get_point(Point3, X3, Y3, Z3),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(3, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X3,Y3,Z3).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point2, _, _, _)),
-	get_point(Point1, X1, Y1, Z1),
-	get_point(Point3, X3, Y3, Z3),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(12, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X1,Y1,Z1),
+	point_constraint(X2,Y2,Z2).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point3, _, _, _)),
-	get_point(Point2, X2, Y2, Z2),
-	get_point(Point1, X1, Y1, Z1),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(13, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X1,Y1,Z1),
+	point_constraint(X3,Y3,Z3).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point1, _, _, _)),
-	not(get_point(Point2, _, _, _)),
-	get_point(Point3, X3, Y3, Z3),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(23, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X2,Y2,Z2),
+	point_constraint(X3,Y3,Z3).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point1, _, _, _)),
-	not(get_point(Point3, _, _, _)),
-	get_point(Point2, X2, Y2, Z2),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+det_coords(123, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type) :-
+	det_angle(Type, Angle),
+	point_constraint(X1,Y1,Z1),
+	point_constraint(X2,Y2,Z2),
+	point_constraint(X3,Y3,Z3).
+
+%determinefinger - set X,Y,Z of missing points based on point connections
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	get_coords(Point1, X1, Y1, Z1),
+	get_coords(Point2, X2, Y2, Z2),
+	get_coords(Point3, X3, Y3, Z3).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point2, _, _, _)),
-	not(get_point(Point3, _, _, _)),
-	get_point(Point1, X1, Y1, Z1),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point1, _, _, _)),
+	get_coords(Point2, X2, Y2, Z2),
+	get_coords(Point3, X3, Y3, Z3),
+	det_coords(1, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
 	
-determinefinger(Point1, Point2, Point3, Type, PointList) :-
-	not(get_point(Point1, _, _, _)),
-	not(get_point(Point2, _, _, _)),
-	not(get_point(Point3, _, _, _)),
-	det_coords(X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type),
-	add_to_list(PointList, [[X1,Y1],[X2,Y2],[X3,Y3]], PointList).
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point2, _, _, _)),
+	get_coords(Point1, X1, Y1, Z1),
+	get_coords(Point3, X3, Y3, Z3),
+	det_coords(2, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
+	
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point3, _, _, _)),
+	get_coords(Point2, X2, Y2, Z2),
+	get_coords(Point1, X1, Y1, Z1),
+	det_coords(3, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
+	
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point1, _, _, _)),
+	not(get_coords(Point2, _, _, _)),
+	get_coords(Point3, X3, Y3, Z3),
+	det_coords(12, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
+	
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point1, _, _, _)),
+	not(get_coords(Point3, _, _, _)),
+	get_coords(Point2, X2, Y2, Z2),
+	det_coords(13, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
+	
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point2, _, _, _)),
+	not(get_coords(Point3, _, _, _)),
+	get_coords(Point1, X1, Y1, Z1),
+	det_coords(23, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
+	
+determinefinger(Point1, Point2, Point3, Type,
+	[X1,Y1,Z1], [X2,Y2,Z2], [X3,Y3,Z3]) :-
+	not(get_coords(Point1, _, _, _)),
+	not(get_coords(Point2, _, _, _)),
+	not(get_coords(Point3, _, _, _)),
+	det_coords(123, X1, Y1, Z1, X2, Y2, Z2, X3, Y3, Z3, Type).
 	
