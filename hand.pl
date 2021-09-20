f82848bd18cfbd4ca7175e49bd45820ac578a966
Anton Sorokin <sgcube@yandex.ru>
2021-09-20 21:33:27 +0300

    structures for hand and fingers

--- hand.pl
+++ hand.pl
@@ -1,41 +1,61 @@
-:- module(hand,[finger_type/4,valid_angle/2,angle_det_type/2]).
+:- module(hand,[
+	point/3, point_list/2, hand/7,finger_angle/4,
+	valid_angle/3, valid_cos_angle/3,
+	angle_det_type/2, angle_type_limits/3,
+]).
 :- use_module(library(clpfd)).
 
-%finger type connection (P1, P2, P3, Type)
-%P1,P2,P3 in [1;42]
+point(X, Y, Z).
+point_list([X, Y, Z], point(X, Y, Z)).
 
-finger_type(1, 2, 3, bpabc).
-finger_type(4, 5, 6, oabc).
-finger_type(5, 6, 7, obcd).
-finger_type(8, 9, 10, oabc).
-finger_type(9, 10, 11, obcd).
-finger_type(12, 13, 14, oabc).
-finger_type(13, 14, 15, obcd).
-finger_type(16, 17, 18, oabc).
-finger_type(17, 18, 19, obcd).
+hand(
+	finger(little, P0, P1, P2, P3),		%5|finger V
+	finger(ring, P4, P5, P6, P7),		%4|finger IV
+	finger(middle, P8, P9, P10, P11),	%3|finger III
+	finger(index, P12, P13, P14, P15),	%2|finger II
+	finger(thumb, P16, P17, P18),		%1|finger I
+	P19, P20							%base
+).
 
-finger_type(2, 3, 21, bpprived).
-finger_type(4, 7, 21, oprived).
-finger_type(8, 11, 21, oprived).
-finger_type(12, 15, 21, oprived).
-finger_type(16, 19, 21, oprived).
-finger_type(1, 2, 3, bppsgib1).
-finger_type(2, 3, 21, bppsgib2).
-finger_type(5, 4, 6, o2sgib1).
-finger_type(6, 5, 7, o2sgib2).
-finger_type(7, 6, 21, o2sgib3).
-finger_type(9, 8, 10, o3sgib1).
-finger_type(10, 9, 11, o3sgib2).
-finger_type(11, 10, 21, o3sgib3).
-finger_type(13, 12, 14, o4sgib1).
-finger_type(14, 13, 15, o4sgib2).
-finger_type(15, 14, 21, o4sgib3).
-finger_type(17, 16, 18, o5sgib1).
-finger_type(18, 17, 19, o5sgib2).
-finger_type(19, 18, 21, o5sgib3).
+finger_angle(little, x, MinAngle, MaxAngle).
+finger_angle(little, y, MinAngle, MaxAngle).
+finger_angle(little, z, MinAngle, MaxAngle).
+
+finger_angle(ring, x, MinAngle, MaxAngle).
+finger_angle(ring, y, MinAngle, MaxAngle).
+finger_angle(ring, z, MinAngle, MaxAngle).
+
+finger_angle(middle, x, MinAngle, MaxAngle).
+finger_angle(middle, y, MinAngle, MaxAngle).
+finger_angle(middle, z, MinAngle, MaxAngle).
+
+finger_angle(index, x, MinAngle, MaxAngle).
+finger_angle(index, y, MinAngle, MaxAngle).
+finger_angle(index, z, MinAngle, MaxAngle).
+
+finger_angle(thumb, x, MinAngle, MaxAngle).
+finger_angle(thumb, y, MinAngle, MaxAngle).
+finger_angle(thumb, z, MinAngle, MaxAngle).
+
+%valid_angle - check if angle is valid for finger on specific axis
+valid_angle(Finger, Axis, Angle):-
+	finger_angle(Finger, Axis, MinAngle, MaxAngle),
+	MinAngle =< Angle, Angle =< MaxAngle.
+	
+%valid_cos_angle - check if angle cosinus is valid for finger on specific axis
+%valid_cos_angle(Finger, Axis, Cos)
+valid_cos_angle(Finger, Axis, Cos):-
+	finger_angle(Finger, Axis, MinAngle, MaxAngle),
+	MinAngleCos is cos(MinAngle),
+	MaxAngleCos is cos(MaxAngle),
+	MinCos is min(MinAngleCos, MaxAngleCos),
+	MaxCos is max(MaxAngleCos, MinAngleCos),
+	MinCos =< Cos, Cos =< MaxCos.
 
 %angle limits in degrees for fingers
-%angle_type_limits(Type, MinAngle, MaxAngle)
+%angle_type_limits(Finger, MinAngle, MaxAngle)
+
+% old
 
 angle_type_limits(bpabc, -80, 80).
 angle_type_limits(bpbcd, -50, 50).
@@ -65,23 +85,6 @@ angle_type_limits(o4sgib3, -80, 80).
 angle_type_limits(o5sgib1, -120, 90).
 angle_type_limits(o5sgib2, -100, 100).
 angle_type_limits(o5sgib3, -80, 80).
-
-
-%valid_angle - check if angle is valid for type
-%valid_angle(Type, Angle)
-valid_angle(Type, Angle):-
-	angle_type_limits(Type, MinAngle, MaxAngle),
-	MinAngle #=< Angle, Angle #=< MaxAngle.
-	
-%valid_cos_angle - check if angle cosinus is valid for type
-%valid_cos_angle(Type, Cos)
-valid_cos_angle(Type, Cos):-
-	angle_type_limits(Type, MinAngle, MaxAngle),
-	MinAngleCos = cos(MinAngle),
-	MaxAngleCos = cos(MaxAngle),
-	MinCos = min(MinAngleCos, MaxAngleCos),
-	MaxCos = max(MaxAngleCos, MinAngleCos),
-	MinCos #=< Cos, Cos #=< MaxCos.
 
 angle_det_type(bpprived, x).
 angle_det_type(oprived, x).
