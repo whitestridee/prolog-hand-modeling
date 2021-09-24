2487459e25a7294508523c52841e78c8b4615550
Anton Sorokin <sgcube@yandex.ru>
2021-09-24 21:11:11 +0300

    clean up app.py code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from utils.prolog import get_answer
 from utils.files import get_xyz_point, get_edges
 from utils.graphics import create_visual
 
+import json
+
 if __name__ == '__main__':
     # Задание
     hand_coor = []
@@ -13,29 +15,33 @@ if __name__ == '__main__':
         )
         xyz = list(map(lambda x: None if x == 'Nan' else float(x), vertices.split(';')))
         hand_coor.append(xyz)
-
-    hand_coord_arg = ', '.join([str(x) for x in hand_coor])
-
-    print(hand_coor[7])
-    statement = f'validate_all({",".join(map(str, ["Result"] + hand_coor))})'
-    statement1 = ("validate_all('.', Result, " + hand_coord_arg + ")")
-
-    answer1 = get_answer(
-        basestored='validation.pl', statement=statement1,
-    )
-    print('-- Задание 1. Пролог --')
-    print(f'{statement1}')
-    for i in answer1:
-        if i["Result"] != b'Ok':
-            print('Такой кисти не может существовать')
-
-        else:
-            print('Сейчас будет нарисована кисть')
-            break
-
+       
     bone_edges = get_edges(
         filename='bone_edges.json',
         filepath='utils'
     )
+
+    hand_coord_arg = ', '.join([str(x) for x in hand_coor])
+    statement = ("validate_all('.', Result, " + hand_coord_arg + ")")
+
+    answer1 = get_answer(
+        basestored='validation.pl', statement=statement,
+    )
+    
+    print('-- Задание 1. Пролог --')
+    print(f'{statement}')
+    for i in answer1:
+        print(i)
         
-    create_visual(bone_edges, hand_coor)
+
+    with open(f'./points.txt', 'r', encoding='utf-8') as f:
+        filedate = f.read()
+    if filedate is None:
+        print('Координаты кистей корректны')
+    else:
+        filedate = filedate.split('\n')
+        incorrect_coor = []
+        for line in filedate:
+            if line:
+                incorrect_coor += json.loads(line)
+    create_visual(bone_edges, hand_coor, incorrect_coor)
