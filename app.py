647b459a04a537d5dee1aebb4c33c10908a64a8b
Anton Sorokin <sgcube@yandex.ru>
2021-09-30 19:59:04 +0300

    выделение импорта/экспорта в модуль чтения/записи

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import json
 import time
 import tkinter
-from tkinter import filedialog, messagebox
 from OpenGL import GL, GLU
 from pyopengltk import OpenGLFrame
 
-import utils.read_files as rf
-from utils.model import HandModel, RightHandModel
-from utils.vector import Vector3
+from utils.io import import_file, export_file
 from utils.prolog import get_answer
 
 from PIL import ImageTk, Image
@@ -169,44 +166,6 @@ def key_scale(event):
         GL.glTranslatef(0, -0.5, 0)
     if event.keysym == "Down":
         GL.glTranslatef(0, 0.5, 0)
-
-
-def import_file():
-    path = filedialog.askopenfilename(
-        defaultextension=".txt",
-        filetypes=(
-            ("Txt Files", "*.txt"),
-            ("All Files", "*.*")
-        )
-    )
-    if path[-4:] != ".txt" and len(path):
-        messagebox.showinfo(message="Неподходящий тип файла",
-                            title="Ошибка!")
-    elif len(path):
-        Scene.vertices = rf.get_vertices(path)
-        Scene.edges = rf.get_edges('utils/bone_edges.json')
-
-        vec_hand_coord = [Vector3(x[0], x[1], x[2]) for x in Scene.vertices]
-        Scene.mesh_left = HandModel(vec_hand_coord[:21])
-        Scene.mesh_right = RightHandModel(vec_hand_coord[21:])
-
-        Scene.Source.vertices = Scene.vertices.copy()
-        Scene.incorrect_coord.clear()
-        Scene.Source.incorrect_coord.clear()
-
-
-def export_file():
-    f = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
-    if f is None:
-        return
-
-    vertices = Scene.vertices
-    text = ''
-    for point in vertices:
-        text += ';'.join([str(axis) for axis in point]) + '\n'
-
-    f.write(text)
-    f.close()
 
 
 def update_mesh():
