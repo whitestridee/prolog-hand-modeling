4dc89b41fc007527a83fdb9d8fa09a3c25c45143
kolomiecartiom <57214287+kolomiecartiom@users.noreply.github.com>
2021-09-30 17:00:13 +0300

    Покурим? (2) И редактирование точек

--- tk.py
+++ tk.py
@@ -1,4 +1,3 @@
-import json
 import time
 import tkinter
 from tkinter import filedialog, messagebox
@@ -7,17 +6,12 @@ from pyopengltk import OpenGLFrame
 
 import utils.read_files as rf
 from utils.model import HandModel, RightHandModel
-from utils.prolog import get_answer
 from utils.vector import Vector3
 
 from PIL import ImageTk, Image
 
 
 class Scene:
-    class Source:
-        vertices = []
-        incorrect_coord = []
-
     edges = []
     vertices = []
     incorrect_coord = []
@@ -26,6 +20,7 @@ class Scene:
     mesh_left = None
     mesh_right = None
     mesh = True
+    edit_point = None
 
     @classmethod
     def set_hands(cls, edges, vertices, incorrect_coord,
@@ -142,21 +137,22 @@ def hands(edges, verticies, incorrect_coor, mesh_left, mesh_right, mesh):
 def mouse_motion(event):
     Scene.mouse_x = event.x
     Scene.mouse_y = event.y
+    # print(Scene.mouse_x, Scene.mouse_y)
+
 
 def camera_motion(event):
     rel_x = event.x - Scene.mouse_x
     rel_y = event.y - Scene.mouse_y
     GL.glRotatef(rel_y, 1, 0, 0)
     GL.glRotatef(rel_x, 0, 1, 0)
+    #print(rel_x, rel_y)
     mouse_motion(event)
-
 
 def mouse_scale(event):
     if event.num == 5 or event.delta == -120:
         GL.glScalef(0.5, 0.5, 0.5)
     if event.num == 4 or event.delta == 120:
         GL.glScalef(1.5, 1.5, 1.5)
-
 
 def key_scale(event):
     if event.keysym == "Left":
@@ -185,13 +181,8 @@ def import_file():
         Scene.edges = rf.get_edges('utils/bone_edges.json')
 
         vec_hand_coord = [Vector3(x[0], x[1], x[2]) for x in Scene.vertices]
-
         Scene.mesh_left = HandModel(vec_hand_coord[:21])
         Scene.mesh_right = RightHandModel(vec_hand_coord[21:])
-
-        Scene.Source.vertices = Scene.vertices.copy()
-        Scene.incorrect_coord.clear()
-        Scene.Source.incorrect_coord.clear()
 
 
 def export_file():
@@ -212,59 +203,88 @@ def update_mesh():
     Scene.mesh = not Scene.mesh
     print(Scene.mesh)
 
-
-def valid_points():
-
-    hand_coord_arg = ', '.join([str(x) for x in Scene.Source.vertices])
-    statement = ("validate_all('.', Result, " + hand_coord_arg + ")")
-
-    answer1 = get_answer(
-        basestored='validation.pl', statement=statement,
-    )
-
-    Scene.Source.incorrect_coord.clear()
-
-    with open(f'./points.txt', 'r', encoding='utf-8') as f:
-        filedate = f.read()
-    if filedate is None:
-        print('Координаты кистей корректны')
-    else:
-        filedate = filedate.split('\n')
-        for line in filedate:
-            if line:
-                Scene.Source.incorrect_coord += json.loads(line)
-
-    Scene.incorrect_coord = Scene.Source.incorrect_coord.copy()
-
-
 def take_vertex(event):
-    print(Scene.mouse_x, Scene.mouse_y)
-    pass
-    # projection = GL.glGetDoublev(GL.GL_PROJECTION_MATRIX)
-    # viewport = GL.glGetIntegerv(GL.GL_VIEWPORT)
-    # modelview = Scene.modelMatrix
-    #
-    # winX = float()
-    # winY = float(viewport[3]) - float(Scene.mouse_y)
-    # posXF, posYF, posZF = GLU.gluUnProject(winX, winY, 1, model=modelview, proj=projection, view=viewport)
-    # posXN, posYN, posZN = GLU.gluUnProject(winX, winY, 0, model=modelview, proj=projection, view=viewport)
-    #
-    # posZ = 0
-    # posX = (posZ - posZN) / (posZF - posZN) * (posXF - posXN) + posXN
-    # posY = (posZ - posZN) / (posZF - posZN) * (posYF - posYN) + posYN
-    #
-    # print(posX, posY, posXF, posYF)
-    # #print(Scene.vertices)
+    vertices_img = [
+        [11, 55],
+        [18, 72],
+        [25, 87],
+        [34, 105],
+        [47, 23],
+        [49, 45],
+        [51, 68],
+        [54, 88],
+        [77, 9],
+        [79, 34],
+        [76, 60],
+        [77, 82],
+        [115, 25],
+        [113, 43],
+        [108, 64],
+        [101, 84],
+        [148, 105],
+        [136, 122],
+        [117, 143],
+        [71, 126],
+        [66, 190],
+        [179, 105],
+        [193, 126],
+        [209, 142],
+        [209, 22],
+        [211, 42],
+        [217, 64],
+        [221, 83],
+        [245, 11],
+        [245, 32],
+        [247, 57],
+        [247, 81],
+        [276, 21],
+        [276, 45],
+        [273, 66],
+        [269, 86],
+        [313, 53],
+        [306, 74],
+        [299, 89],
+        [290, 106],
+        [253, 125],
+        [259, 190],
+    ]
+
+    for item_points in range(len(vertices_img)):
+        if event.x - 4 < vertices_img[item_points][0] < event.x + 4:
+            if event.y - 4 < vertices_img[item_points][1] < event.y + 4:
+                Scene.edit_point = item_points
+                break
+
+def edit_points(move):
+    print(Scene.vertices[Scene.edit_point][0])
+    if Scene.edit_point is not None:
+        if move == 'X+':
+            Scene.vertices[Scene.edit_point][0] += 5
+        if move == 'X-':
+            Scene.vertices[Scene.edit_point][0] -= 5
+        if move == 'Y+':
+            Scene.vertices[Scene.edit_point][1] += 5
+        if move == 'Y-':
+            Scene.vertices[Scene.edit_point][1] -= 5
+        if move == 'Z+':
+            Scene.vertices[Scene.edit_point][2] += 5
+        if move == 'Z-':
+            Scene.vertices[Scene.edit_point][2] -= 5
+    # print(Scene.vertices[Scene.edit_point][0])
 
 
-def app_main():
+def app_main(edges, vertices, incorrect_coord, mesh_left, mesh_right):
+    vertex, incorrect_coord = transform_coord(vertices, incorrect_coord,
+                                              mesh_left, mesh_right)
     screen = (1200, 700)
+
+    Scene.set_hands(edges, vertices, incorrect_coord, mesh_left, mesh_right)
 
     root = tkinter.Tk()
     root.geometry(f'{screen[0]}x{screen[1]}')
 
     app = AppOgl(root, width=screen[0], height=screen[1])
-    app.place(relheight=1, relwidth=0.8)
+    app.place(relheight=1, relwidth=0.7)
 
     btn_import = tkinter.Button(
         root,
@@ -273,7 +293,7 @@ def app_main():
         pady="6",
         command=import_file
     )
-    btn_import.place(relx=0.81, rely=0.03)
+    btn_import.place(relx=0.76, rely=0.03)
 
     btn_export = tkinter.Button(
         root,
@@ -282,77 +302,79 @@ def app_main():
         pady="6",
         command=export_file,
     )
-    btn_export.place(relx=0.81, rely=0.09)
+    btn_export.place(relx=0.76, rely=0.09)
 
     btn_check = tkinter.Button(
         root,
         text="Проверить точки",
         padx="60",
-        pady="6",
-        command=valid_points
+        pady="6"
     )
-    btn_check.place(relx=0.81, rely=0.15)
+    btn_check.place(relx=0.76, rely=0.15)
 
     show_edges = tkinter.Checkbutton(text="Отобразить мэш", command=update_mesh)
-    show_edges.place(relx=0.81, rely=0.21)
+    show_edges.place(relx=0.76, rely=0.21)
     show_edges.select()
 
     img = ImageTk.PhotoImage(Image.open("img_hands_small.jpg"))
     panel = tkinter.Label(root, image=img)
-    panel.place(relx=0.81, rely=0.28, height=154, width=220)
+    panel.place(relx=0.71, rely=0.28, height=235, width=335)
 
-    label_point = tkinter.Label(text = "No Point Selected", font = 'Times 20')
-    label_point.place(relx = 0.81, rely = 0.47)
-    label_entry = tkinter.Label(text="Шаг:", font='Times 20')
-    label_entry.place(relx=0.81, rely=0.53)
-    message_entry = tkinter.Entry(width=5, font='Times 20')
-    message_entry.place(relx=0.88, rely=0.53)
+    label_entry = tkinter.Label(text="Шаг:", font='Times 14')
+    label_entry.place(relx=0.71, rely=0.63)
+    message_entry = tkinter.Entry(width=10, font='Times 14')
+    message_entry.place(relx=0.78, rely=0.63)
 
-    label_x = tkinter.Label(text="Ось X:", font='Times 20')
-    label_x.place(relx=0.81, rely=0.62)
+    label_x = tkinter.Label(text="Ось X:", font='Times 14')
+    label_x.place(relx=0.71, rely=0.70)
     btn_plus_x = tkinter.Button(
         root,
         text="+",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='X+': edit_points(arg)
     )
-    btn_plus_x.place(relx=0.88, rely=0.62)
+    btn_plus_x.place(relx=0.76, rely=0.70)
     btn_minus_x = tkinter.Button(
         root,
         text="-",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='X-': edit_points(arg)
     )
+    btn_minus_x.place(relx=0.82, rely=0.70)
 
-    btn_minus_x.place(relx=0.91, rely=0.62)
-
-    label_y = tkinter.Label(text="Ось Y:", font='Times 20')
-    label_y.place(relx=0.81, rely=0.70)
+    label_y = tkinter.Label(text="Ось Y:", font='Times 14')
+    label_y.place(relx=0.71, rely=0.75)
     btn_plus_y = tkinter.Button(
         root,
         text="+",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='Y+': edit_points(arg)
     )
-    btn_plus_y.place(relx=0.88, rely=0.70)
+    btn_plus_y.place(relx=0.76, rely=0.75)
     btn_minus_y = tkinter.Button(
         root,
         text="-",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='Y-': edit_points(arg)
     )
-    btn_minus_y.place(relx=0.91, rely=0.70)
+    btn_minus_y.place(relx=0.82, rely=0.75)
 
-    label_z = tkinter.Label(text="Ось Z:", font='Times 20')
-    label_z.place(relx=0.81, rely=0.80)
+    label_z = tkinter.Label(text="Ось Z:", font='Times 14')
+    label_z.place(relx=0.71, rely=0.80)
     btn_plus_z = tkinter.Button(
         root,
         text="+",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='Z+': edit_points(arg)
     )
-    btn_plus_z.place(relx=0.88, rely=0.80)
+    btn_plus_z.place(relx=0.76, rely=0.80)
     btn_minus_z = tkinter.Button(
         root,
         text="-",
-        font='Times 15'
+        font='Times 13',
+        command=lambda arg='Z-': edit_points(arg)
     )
-    btn_minus_z.place(relx=0.91, rely=0.80)
+    btn_minus_z.place(relx=0.82, rely=0.80)
 
     app.bind("<Motion>", mouse_motion)
     app.bind("<B1-Motion>", camera_motion)
@@ -368,4 +390,4 @@ def app_main():
 
 
 if __name__ == '__main__':
-    app_main()
+    app_main([], [], [], None, None)
